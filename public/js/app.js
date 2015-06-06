dc58ec64109bc5ce7a0e2db15f54c377cf88cc89
hz7680 <346187456@qq.com>
2015-06-06 09:39:48 +0800

    添加特效
    添加特效

--- public/js/app.js
+++ public/js/app.js
@@ -35,11 +35,34 @@ $(function(){
         $(this).css("background","none").css("color","#ffffff");
         $(this).parent().animate({width:"80%"},200);
     })
+    //nav伸缩按钮控制
+    $("#nav").hover(function(){
+        $("#nav_btn_close").css("display","block");
+    },function(){
+        $("#nav_btn_close").css("display","none");
+    })
+    //按钮实现伸缩
+    $("#nav_btn_close a").click(function(){
+        $("#nav").animate({left:"-15%"},500,function(){
+            $("#nav_btn_close").css("display","none");
+            $("#nav_btn_open").css("display","block");
+        });
+        $("#right_box").animate({width:"100%",marginLeft:"0"},500)
+    })
+    $("#nav_btn_open a").click(function(){
+        $("#nav").animate({left:0},500,function(){
+            $("#nav_btn_close").css("display","block");
+            $("#nav_btn_open").css("display","none");
+        });
+        $("#right_box").animate({width:"85%",marginLeft:"15%"},500)
+    })
 
     function init(){
         var height=$("#nav .navlist li").eq(0).height();
         $("#nav .navlist li a").css("line-height",height+"px");
         $("#nav .navlist li").fadeOut(0);
+        var nav_btn_height=$("#nav_btn_close").height();
+        $("#nav .nav_btn a").css("lineHeight",nav_btn_height+"px");
     }
     $(window).resize(function(){
         init();
